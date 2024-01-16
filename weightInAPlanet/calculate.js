cb073aa4f432d437641459328b4f7616f568e586
Brandt322 <bgamboaa98@gmail.com>
2024-01-16 12:34:38 -0500

    feature: Added logic for serach button, validation input and options

--- weightInAPlanet/calculate.js
+++ weightInAPlanet/calculate.js
@@ -1,10 +1,43 @@
 import { gravities } from "./gravities"
 export const calculateWeight = (element) => {
+
   const planetInfo = document.getElementById('planet-info')
-  const span = planetInfo.querySelector('span')
+  const pInfo = planetInfo.querySelector('p')
+  const spanInfo = planetInfo.querySelector('span')
+  const weight = planetInfo.querySelector('#weight')
+  const massRequired = document.getElementById('massRequired')
+
   element.addEventListener('click', () => {
-    let planet = document.getElementById('planet').value
-    planet !== 'none' ? span.textContent = planet : span.textContent = 'miau'
-    console.log(planet)
+    const mass = document.getElementById('mass')
+    const planet = document.getElementById('planet').value
+    const message = massRequired.querySelector('p')
+    const img = document.querySelector('img')
+
+    if (mass.value !== '' && planet !== 'none') {
+      planetInfo.classList.remove('hidden')
+      massRequired.classList.add('hidden')
+
+      pInfo.firstChild.textContent = 'The weight of the object on '
+      for (const gravity in gravities) {
+        if (planet === gravity) {
+          spanInfo.textContent = planet
+          weight.textContent = `${gravities[planet] * mass.value} N`
+          img.setAttribute('src', `./assets/${planet.toLowerCase()}.png`)
+        }
+      }
+    }
+
+    if (mass.value !== '' && planet === 'none') {
+      planetInfo.classList.add('hidden')
+      massRequired.classList.remove('hidden')
+      message.textContent = 'You also need to select a planet'
+    }
+
+    if (mass.value === '' && planet !== 'none') {
+      planetInfo.classList.add('hidden')
+      massRequired.classList.remove('hidden')
+      message.textContent = 'Mass is required'
+    }
+
   })
 }
--- weightInAPlanet/main.js
+++ weightInAPlanet/main.js
@@ -2,10 +2,10 @@ import './style.css'
 import { calculateWeight } from './calculate'
 
 document.querySelector('#app').innerHTML = `
-<header class="w-1/2 justify-start">
+<header class="w-1/2">
   <h1 class="text-5xl w-full mt-6 mb-16">Calculate a weight of an object on a planet </h1>
 
-  <input class="p-1 border-solid border-2 border-sky-500 text-gray-500 mr-4 text-lg" type="text" id="mass" placeholder="Mass in Kilogram" />
+  <input class="p-1 border-solid border-2 border-sky-500 text-gray-500 mr-4 text-lg" type="number" id="mass" placeholder="Mass in Kilogram" />
   <select id="planet" class="p-1 border-solid border-2 border-sky-500 mr-4 text-lg text-gray-500">
     <option class="border-solid" value='none'> --Select planet-- </option>
     <option class="border-solid" value='Mercury'>Mercury</option>
@@ -17,18 +17,18 @@ document.querySelector('#app').innerHTML = `
   <button class="bg-sky-800 p-1 border-solid border-2 border-sky-500 mr-4 text-lg" id="calculate" type="button">Calculate weight</button>
 </header>
 
-<main id="planet-info" class="w-1/2 justify-end">
-  <div class="flex items-center gap-4 justife-betweend p-4 w-full">
+<main class="w-1/2">
+  <div id="planet-info" class="flex items-center gap-4 justife-betweend p-4 w-full hidden">
     <div class="w-1/2">
       <img class="w-96" src='./assets/earth.png' />
     </div>
     <div class="flex flex-col bg-[#242424] border-solid rounded-lg p-6 w-1/2 text-2xl items-center gap-4">
       <p>Texto de prueba <span class="text-white font-bold">Pluton</span></p>
-      <div class="bg-[#454646] rounded-[50%] flex justify-center items-center  w-32 h-32 text-white font-bold text-center">7.00000 N</div>
+      <div class="bg-[#454646] rounded-[50%] flex justify-center items-center min-w-32 h-32 p-2 text-white font-bold text-center"  id="weight">7.00000 N</div>
     </div>
   </div>
 
-  <div class="flex justify-center rounded-md bg-[#242424] p-4 hidden">
+  <div id="massRequired" class="flex justify-center rounded-md bg-[#242424] p-4">
     <p class="text-4xl text-center rounded-md bg-[#454646] p-4">Mass is required</p>
   </div>
 </main>
