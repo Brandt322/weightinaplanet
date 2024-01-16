6d7914007d9fc79f7d041bbe52a9be6a009c14df
Jean Mendoza <smilu.u@hotmail.com>
2024-01-16 12:39:49 -0500

    new pene

--- weightInAPlanet/main.js
+++ weightInAPlanet/main.js
@@ -5,8 +5,8 @@ document.querySelector('#app').innerHTML = `
 <header class="w-1/2 justify-start">
   <h1 class="text-5xl w-full mt-6 mb-16">Calculate a weight of an object on a planet </h1>
 
-  <input class="p-1 border-solid border-2 border-sky-500 text-gray-500 mr-4 text-lg" type="text" id="mass" placeholder="Mass in Kilogram" />
-  <select class="p-1 border-solid border-2 border-sky-500 mr-4 text-lg text-gray-500	">
+  <input id= "mass" class="p-1 border-solid border-2 border-sky-500 text-gray-500 mr-4 text-lg" type="number" id="mass" placeholder="Mass in Kilogram" />
+  <select id = "planet" class="p-1 border-solid border-2 border-sky-500 mr-4 text-lg text-gray-500	">
     <option class="border-solid" value='none'> --Select planet-- </option>
     <option class="border-solid" value='Mercury'>Mercury</option>
     <option class="border-solid" value='Venus'>Venus</option>
@@ -20,11 +20,12 @@ document.querySelector('#app').innerHTML = `
 <main class="w-1/2 justify-end">
   <div class="flex items-center gap-4 justife-betweend p-4 w-full">
     <div class="w-1/2">
-      <img class="w-96" src='./assets/earth.png' />
+      <img id="imgPlanets" class="w-96" src='./assets/earth.png' />
     </div>
+    
     <div class="flex flex-col bg-[#242424] border-solid rounded-lg p-6 w-1/2 text-2xl items-center gap-4">
-      <p>Texto de prueba <span class="text-white font-bold">Pluton</span></p>
-      <div class="bg-[#454646] rounded-[50%] flex justify-center items-center  w-32 h-32 text-white font-bold text-center">7.00000 N</div>
+      <p id="description">Texto de prueba </p><span id="spanPlanet" class="text-white font-bold">Pluton</span>
+      <div id="weigth" class="bg-[#454646] rounded-[50%] flex justify-center items-center  w-32 h-32 text-white font-bold text-center">7.00000 N</div>
     </div>
   </div>
 
@@ -32,6 +33,6 @@ document.querySelector('#app').innerHTML = `
     <p class="text-4xl text-center rounded-md bg-[#454646] p-4">Mass is required</p>
   </div>
 </main>
-`
+`//CAMBIOS AL SPAN
 
 calculateWeight(document.getElementById('calculate'))
