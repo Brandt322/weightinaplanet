e2db865cf72b4ea02a064d005d487059c9db7cfe
Brandt322 <bgamboaa98@gmail.com>
2024-01-16 00:44:24 -0500

    add images and styles to main.js

--- weightInAPlanet/main.js
+++ weightInAPlanet/main.js
@@ -1,24 +1,27 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1 class="text-3xl font-bold underline">Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+<header class="w-1/2 justify-start">
+  <h1 class="text-5xl w-full mt-6 mb-16">Calculate a weight of an object on a planet</h1>
 
-setupCounter(document.querySelector('#counter'))
+  <input type="text" id="mass" placeholder="Mass in Kilogram" />
+  <select>
+    <option value='none'> --select planet-- </option>
+  </select>
+  <button>Calculate weight</button>
+</header>
+
+<main class="w-1/2 justify-end">
+  <div class="flex hidden">
+    <div>
+      <img src='./assets/earth.png' />
+    </div>
+    <div class="hidden"></div>
+  </div>
+  <div class="flex justify-center rounded-md bg-[#242424] p-4">
+    <p class="text-4xl text-center rounded-md bg-[#454646] p-4">Mass is required</p>
+  </div>
+</main>
+`
