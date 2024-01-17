9d4ba9bae37a5dc334c78536b646ed36f76f50c0
Jean Mendoza <smilu.u@hotmail.com>
2024-01-17 18:07:55 -0500

    day 24 completed

--- weightInAPlanet/calculate.js
+++ weightInAPlanet/calculate.js
@@ -1,29 +1,46 @@
 import { gravities } from "./gravities"
 
 export const calculateWeight = (element) => {
+  const hidden = document.getElementById('content-d-w')
+  const hidden2 = document.getElementById('content-fail')
+  const p = document.getElementById('p-fail')
+
   const mass = document.getElementById('mass')
+  const weight = document.getElementById('weigth')
+  const description = document.getElementById('description')
   const imgPlanets = document.getElementById('imgPlanets')
   const spanPlanet = document.getElementById('spanPlanet')
-  const description = document.getElementById('description')
-  const weight = document.getElementById('weigth')
-
+  const planet = document.getElementById('planet')
+  
+  
   element.addEventListener('click', () => {
     let weightTotal 
     for( let value in gravities) {
       if(value == planet.value){
         weightTotal = gravities[value] * mass.value
-        console.log(weightTotal)
       }
     }
-    // ./assets/earth.png
-    if(planet.value){
-      imgPlanets.src = `./assets/${planet.value}.png`
-    }
     
-    description.textContent = 'The weight of the object on'
-    spanPlanet.textContent = planet.value
-    console.log(planet.value)
-    weight.textContent = weightTotal
+    if(planet.value !=='none' && mass.value !== '' ){
+      imgPlanets.classList.remove('hidden')
+      hidden.classList.remove('hidden')
+      imgPlanets.src = `./assets/${planet.value}.png`
+      description.textContent = 'The weight of the object on'
+      spanPlanet.textContent = planet.value
+      weight.textContent = weightTotal
+    } 
+
+    if(planet.value=='none'&& mass.value !==''){
+      hidden2.classList.remove('hidden')
+      hidden.classList.add('hidden')
+      p.textContent = 'You also need to select a planet'
+    } 
+
+    if(planet.value !=='none'&& mass.value ==''){
+      hidden2.classList.remove('hidden')
+      hidden.classList.add('hidden')
+      p.textContent = 'Mass is required'
+    }
 
   })
 }
--- weightInAPlanet/main.js
+++ weightInAPlanet/main.js
@@ -13,6 +13,11 @@ document.querySelector('#app').innerHTML = `
     <option class="border-solid" value='Earth'>Earth</option>
     <option class="border-solid" value='Moon'>Moon</option>
     <option class="border-solid" value='Mars'>Mars</option>
+    <option class="border-solid" value='Uranus'>Uranus</option>
+    <option class="border-solid" value='Saturn'>Saturn</option>
+    <option class="border-solid" value='Pluto'>Pluto</option>
+    <option class="border-solid" value='Neptune'>Neptune</option>
+    <option class="border-solid" value='Jupiter'>Jupiter</option>
   </select>
   <button class="bg-sky-800 p-1 border-solid border-2 border-sky-500 mr-4 text-lg" id="calculate">Calculate weight</button>
 </header>
@@ -20,17 +25,17 @@ document.querySelector('#app').innerHTML = `
 <main class="w-1/2 justify-end">
   <div class="flex items-center gap-4 justife-betweend p-4 w-full">
     <div class="w-1/2">
-      <img id="imgPlanets" class="w-96" src='./assets/earth.png' />
+      <img id="imgPlanets" class="w-96 hidden" src='./assets/earth.png' />
     </div>
     
-    <div class="flex flex-col bg-[#242424] border-solid rounded-lg p-6 w-1/2 text-2xl items-center gap-4">
+    <div id="content-d-w" class="hidden flex flex-col bg-[#242424] border-solid rounded-lg p-6 w-1/2 text-2xl items-center gap-4">
       <p id="description">Texto de prueba </p><span id="spanPlanet" class="text-white font-bold">Pluton</span>
       <div id="weigth" class="bg-[#454646] rounded-[50%] flex justify-center items-center  w-32 h-32 text-white font-bold text-center">7.00000 N</div>
     </div>
   </div>
 
-  <div class="flex justify-center rounded-md bg-[#242424] p-4 hidden">
-    <p class="text-4xl text-center rounded-md bg-[#454646] p-4">Mass is required</p>
+  <div id="content-fail" class="flex justify-center rounded-md bg-[#242424] p-4 hidden">
+    <p id="p-fail" class="text-4xl text-center rounded-md bg-[#454646] p-4"></p>
   </div>
 </main>
 `//CAMBIOS AL SPAN
